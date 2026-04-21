2ebfd7220974fc52f565c1029ba2a6135aebdd02
phzineasygame <pedroflorencio@colegiopaulodetarso.com.br>
2026-04-21 19:19:23 +0000

    página de contato concluida, com textos, fotos e direcionadores para minhas redes sociais e gmail

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 window.addEventListener("scroll", () => {
-    const imagem = document.querySelector(".foto");
+    const imagem = document.querySelector(".foto-pc");
     let scroll = window.scrollY;
 
     let escala = 1 + scroll * 0.0005;
 
-    escala = Math.min(escala, 1.1); 
+    escala = Math.min(escala, 1.2); 
 
     imagem.style.transform = `scale(${escala})`;
 });
